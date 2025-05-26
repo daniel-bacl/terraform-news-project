0188908cffcd976811b92cfb1dd85a9d4698b8ad
daniel-bacl <95eksldpf@gmail.com>
2025-05-26 17:44:45 +0900

    add igw & fix dev/main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -4,12 +4,22 @@ module "vpc" {
 
 module "subnet" {
   source = "../../modules/networking/subnet"
+  vpc_id = module.vpc.vpc_id
+}
+
+module "igw" {
+  source = "../../modules/networking/igw"
+  vpc_id = module.vpc.vpc_id
 }
 
 module "route" {
-  source = "../../modules/networking/route"
+  source             = "../../modules/networking/route"
+  vpc_id             = module.vpc.vpc_id
+  igw_id             = module.igw.igw_id
+  public_subnet_ids  = module.subnet.public_subnet_ids
 }
 
 module "s3_backend" {
   source = "../../modules/bootstrap/s3-backend"
 }
+
