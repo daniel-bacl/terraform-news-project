75972cdcaa4e2a6a189a6c91ae0baea7c415e4d6
daniel-bacl <95eksldpf@gmail.com>
2025-05-26 17:31:20 +0900

    edit networkinf file

--- modules/networking/subnet/main.tf
+++ modules/networking/subnet/main.tf
@@ -1,43 +1,55 @@
-# subnet.tf
+variable "vpc_id" {
+  type = string
+}
+
 resource "aws_subnet" "public_1a" {
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = var.vpc_id
   cidr_block              = "192.168.1.0/24"
   availability_zone       = "ap-northeast-2a"
   map_public_ip_on_launch = true
-
   tags = {
     Name = "public-subnet-1a"
   }
 }
 
 resource "aws_subnet" "public_2c" {
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = var.vpc_id
   cidr_block              = "192.168.2.0/24"
   availability_zone       = "ap-northeast-2c"
   map_public_ip_on_launch = true
-
   tags = {
     Name = "public-subnet-2c"
   }
 }
 
 resource "aws_subnet" "private_3b" {
-  vpc_id            = aws_vpc.main.id
+  vpc_id            = var.vpc_id
   cidr_block        = "192.168.3.0/24"
   availability_zone = "ap-northeast-2b"
-
   tags = {
     Name = "private-subnet-3b"
   }
 }
 
 resource "aws_subnet" "private_4d" {
-  vpc_id            = aws_vpc.main.id
+  vpc_id            = var.vpc_id
   cidr_block        = "192.168.4.0/24"
   availability_zone = "ap-northeast-2d"
-
   tags = {
     Name = "private-subnet-4d"
   }
 }
 
+output "public_subnet_ids" {
+  value = [
+    aws_subnet.public_1a.id,
+    aws_subnet.public_2c.id
+  ]
+}
+
+output "private_subnet_ids" {
+  value = [
+    aws_subnet.private_3b.id,
+    aws_subnet.private_4d.id
+  ]
+}
