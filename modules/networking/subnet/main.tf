957e42b4b0ffa41d520fa34ec01f556ebda51073
daniel-bacl <95eksldpf@gmail.com>
2025-05-27 15:52:54 +0900

    add subnet tag

--- modules/networking/subnet/main.tf
+++ modules/networking/subnet/main.tf
@@ -8,7 +8,9 @@ resource "aws_subnet" "public_1a" {
   availability_zone       = "ap-northeast-2a"
   map_public_ip_on_launch = true
   tags = {
-    Name = "public-subnet-1a"
+    "Name"                                = "public-subnet-1a"
+    "kubernetes.io/role/elb"              = "1"
+    "kubernetes.io/cluster/news-cluster"  = "shared"
   }
 }
 
@@ -18,7 +20,9 @@ resource "aws_subnet" "public_2c" {
   availability_zone       = "ap-northeast-2c"
   map_public_ip_on_launch = true
   tags = {
-    Name = "public-subnet-2c"
+    "Name"                                = "public-subnet-2c"
+    "kubernetes.io/role/elb"              = "1"
+    "kubernetes.io/cluster/news-cluster"  = "shared"
   }
 }
 
@@ -27,7 +31,9 @@ resource "aws_subnet" "private_3b" {
   cidr_block        = "192.168.3.0/24"
   availability_zone = "ap-northeast-2b"
   tags = {
-    Name = "private-subnet-3b"
+    "Name"                                = "private-subnet-3b"
+    "kubernetes.io/role/internal-elb"     = "1"
+    "kubernetes.io/cluster/news-cluster"  = "shared"
   }
 }
 
@@ -36,7 +42,9 @@ resource "aws_subnet" "private_4d" {
   cidr_block        = "192.168.4.0/24"
   availability_zone = "ap-northeast-2d"
   tags = {
-    Name = "private-subnet-4d"
+    "Name"                                = "private-subnet-4d"
+    "kubernetes.io/role/internal-elb"     = "1"
+    "kubernetes.io/cluster/news-cluster"  = "shared"
   }
 }
 
