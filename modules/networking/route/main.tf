918fe30331e85954fe2b33b291cef70ab3f34390
daniel-bacl <95eksldpf@gmail.com>
2025-05-27 17:29:06 +0900

    init nat & add destroy workflow

--- modules/networking/route/main.tf
+++ modules/networking/route/main.tf
@@ -10,6 +10,10 @@ variable "public_subnet_ids" {
   type = list(string)
 }
 
+variable "nat_gateway_id" {
+  type = string
+}
+
 resource "aws_route_table" "public" {
   vpc_id = var.vpc_id
   route {
@@ -25,4 +29,10 @@ resource "aws_route_table_association" "public" {
   count          = length(var.public_subnet_ids)
   subnet_id      = element(var.public_subnet_ids, count.index)
   route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route" "private_route" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = var.nat_gateway_id
 }
