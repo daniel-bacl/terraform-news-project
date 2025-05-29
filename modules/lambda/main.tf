119b079a6c402745a56f7826958363c74dea97aa
daniel-bacl <95eksldpf@gmail.com>
2025-05-29 17:52:52 +0900

    임시커밋

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -4,6 +4,19 @@ data "archive_file" "lambda_zip" {
   output_path = "${path.module}/sql_init.zip"
 }
 
+data "archive_file" "pymysql_layer_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/layer"
+  output_path = "${path.module}/pymysql_layer.zip"
+}
+
+resource "aws_lambda_layer_version" "pymysql" {
+  filename         = data.archive_file.pymysql_layer_zip.output_path
+  layer_name       = "pymysql-layer"
+  compatible_runtimes = ["python3.11"]
+  source_code_hash = data.archive_file.pymysql_layer_zip.output_base64sha256
+}
+
 resource "aws_lambda_function" "sql_initializer" {
   function_name    = "rds-sql-initializer"
   role             = var.lambda_role_arn
@@ -11,6 +24,10 @@ resource "aws_lambda_function" "sql_initializer" {
   runtime          = "python3.11"
   filename         = data.archive_file.lambda_zip.output_path
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  layers = [
+    aws_lambda_layer_version.pymysql.arn
+  ]
 
   environment {
     variables = {
