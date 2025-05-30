c67962f2635ddf28e49b21d28c63ad4b8d98c81b
daniel-bacl <95eksldpf@gmail.com>
2025-05-30 11:46:34 +0900

    main.py 괄호 수정 (#15)

--- modules/lambda/sql_initializer/main.py
+++ modules/lambda/sql_initializer/main.py
@@ -10,7 +10,7 @@ def lambda_handler(event, context):
         user=os.environ['DB_USER'],
         password=os.environ['DB_PASSWORD'],
         database=os.environ['DB_NAME'],
-        db_port)
+        db_port
     )
 
     cursor = conn.cursor()
