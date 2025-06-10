5884c210480a95a42c9db0e8aded8d23f08d717a
Diana-Len <jintonylove@gmail.com>
2025-06-10 15:06:02 +0900

    app-sg edit

--- modules/networking/security_group/security_groups.tf
+++ modules/networking/security_group/security_groups.tf
@@ -4,6 +4,31 @@ resource "aws_security_group" "app_sg" {
   description = "Common app-level SG"
   vpc_id      = var.vpc_id
 
+  # ✅ 인바운드: SSH (22), HTTP (80), HTTPS (443)
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
