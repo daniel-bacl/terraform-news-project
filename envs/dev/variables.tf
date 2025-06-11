756bc2b735e67561c923450a02fe5c8e98d35ab2
daniel-bacl <95eksldpf@gmail.com>
2025-06-11 17:55:20 +0900

    docker 이미지 태그 추가

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -23,5 +23,5 @@ variable "lambda_sg_id" {
 variable "docker_image_uri" {
   type = string
   sensitive = true
-  default = "635140758252.dkr.ecr.ap-northeast-2.amazonaws.com/news-crawler-lambda2"
+  default = "635140758252.dkr.ecr.ap-northeast-2.amazonaws.com/news-crawler-lambda2:latest"
 }
