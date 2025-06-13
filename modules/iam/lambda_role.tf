9a8bb72dd5b6f380d7e743d6c182a9f25470e41c
oosuoos <oosuoos@gmail.com>
2025-06-13 14:27:46 +0900

    ses_send_email 권한추가

--- modules/iam/lambda_role.tf
+++ modules/iam/lambda_role.tf
@@ -69,3 +69,22 @@ resource "aws_iam_role_policy_attachment" "lambda_vpc_policy_attachment" {
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = aws_iam_policy.lambda_vpc_access_policy.arn
 }
+
+resource "aws_iam_policy" "ses_send_email" {
+  name   = "AllowSESSendEmail"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "ses:SendEmail",
+        Resource = "arn:aws:ses:ap-northeast-2:635140758252:identity/sol-dni.click"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_ses_attach" {
+  role       = aws_iam_role.lambda_sql_initializer_role.name
+  policy_arn = aws_iam_policy.ses_send_email.arn
+}
