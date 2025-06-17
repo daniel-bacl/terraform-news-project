cf703231e9595d3ac720be22bab5b8c23d5ce9af
daniel-bacl <95eksldpf@gmail.com>
2025-06-17 17:18:47 +0900

    delete alb controller

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -6,14 +6,6 @@ output "cluster_endpoint" {
   value = aws_eks_cluster.this.endpoint
 }
 
-output "cluster_ca" {
-  value = aws_eks_cluster.this.certificate_authority[0].data
-}
-
 output "cluster_ca_data" {
   value = aws_eks_cluster.this.certificate_authority[0].data
-}
-
-output "oidc_provider_url" {
-  value = replace(aws_eks_cluster.this.identity[0].oidc[0].issuer, "https://", "")
 }
